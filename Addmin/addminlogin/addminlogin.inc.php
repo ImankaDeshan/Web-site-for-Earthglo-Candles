c8ecd48ba2687d5e99925d8fd9f2daa51a3685a8
ImankaDeshan <imankadeshan2000@gmail.com>
2024-11-16 00:17:18 +0530

    make addmin login form

---
+++ Addmin/addminlogin/addminlogin.inc.php
@@ -0,0 +1,44 @@
+<?php 
+    require_once '../../db.inc.php';
+    $lgerror = NULL;
+
+    if (isset($_POST['addlogin'])) {
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if (empty($username) || empty($password)) {
+            $lgerror = "Username and Password must be required";
+        } 
+        else {
+            $sql = "SELECT * FROM addmin WHERE user_name = ?";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "s", $username);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if (mysqli_num_rows($result) == 1) {
+                $user = mysqli_fetch_assoc($result);
+    
+                if ($user['password'] == $password) {
+                    // Password matches, start session and store username
+                    $_SESSION['user_name'] = $user['username'];
+                    // $_SESSION['addmin_id'] = $userid ['userid'];
+    
+                    // Redirect to the home page
+                    // header("Location: ../Home page/Candles Online Shop.php");
+                    exit;
+                } else {
+                    $lgerror = "Incorrect password.";
+                }
+            } else {
+                $lgerror = "Username not found.";
+            }
+    
+            mysqli_stmt_free_result($stmt);
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+
+?> 
---
+++ Addmin/addminlogin/addminlogin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="addminlogin.css">
+</head>
+<body>
+<?php require_once 'addminlogin.inc.php';?> 
+    <?php
+        if ($lgerror != NUll) {?> <style> .loginerror{display:block; color: #FF5733;} </style> <?php }
+    ?>
+<div class="page-content"> 
+
+
+<video class="video-background" autoplay loop muted> 
+            <source src="../../Images/Addmin/Home/BackgroundVideo.mp4" type="video/mp4">
+    </video>
+    <p class = "main-header"> Only Sellers are able to enter this side </p>
+    <form action="" method = "post" class="login-form">
+        <h1>Login for earthglo </h1> 
+        <h2>  As a Addmin</h2>
+        <p class = "loginerror"> <?php echo $lgerror ?> </p>
+        <input type="text" class="username" name = "username" placeholder = "Username">
+        <input type="password" name = "password" class="password" placeholder = "Password">
+        <button class="login" name = "addlogin" type = "submit"> Login</button>
+    </form>
+</div>
+</body>
+</html>
